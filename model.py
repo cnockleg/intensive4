0365dd1a24a0593080d88d8a8e7388f5fab19f6e
cnockleg <cnockleg1@yandex.ru>
2025-05-24 23:24:12 +0300

    added streamlit api

---
+++ model.py
@@ -0,0 +1,42 @@
+import torch
+from sentence_transformers import SentenceTransformer
+
+from project.modeling import MultiLabelClassifier
+
+class Model():
+    def __init__(self, model_path):
+        self.model = MultiLabelClassifier(embedding_dim=312, num_labels=6)
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.eval()
+
+        self.sbert = SentenceTransformer('cointegrated/rubert-tiny2')
+
+        self.labels = [
+            'Вопрос решен',
+            'Нравится качество выполнения заявки',
+            'Нравится качество работы сотрудников',
+            'Нравится скорость отработки заявок',
+            'Понравилось выполнение заявки',
+            'Вопрос не решен'
+        ]
+
+        self.threshold = [0.31 for label in self.labels]
+
+    def predict(self, text: str):
+        # self.model.eval()
+        with torch.no_grad():
+            emb = self.sbert.encode(text, convert_to_tensor=True).unsqueeze(0)  # добавляем batch dimension
+            preds = self.model(emb).cpu().numpy()[0]
+            preds_bin = (preds >= self.threshold).astype(int)
+            predicted_tags = [label for label, pred in zip(self.labels, preds_bin) if pred == 1]
+        return predicted_tags, preds
+    
+if __name__ == "__main__":
+    model = Model(r'intensiv-3-main\intensive4\intensive4_evgeniy\final_model.pth')
+    text = 'оперативно приехали, все хорошо'
+
+    tags, scores = model.predict(text)
+    
+    print(f"Текст: {text}")
+    print(f"Предсказанные теги: {tags}")
+    print(f"Raw scores: {scores}")
---
+++ streamlit_app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+from model import Model
+
+@st.cache_resource
+def load_model():
+    return Model(r'intensiv-3-main\intensive4\intensive4_evgeniy\final_model.pth')
+
+model = load_model()
+
+st.title("Классификация отзывов по тегам")
+text_input = st.text_area("Введите отызв: ")
+
+if st.button("Предсказать теги", type='primary'):
+    if text_input.strip():
+        tags, scores = model.predict(text_input)
+
+        df_scores = pd.DataFrame({
+            'Тег': model.labels,
+            'Вероятность': [f"{s:.2f}" for s in scores]
+        })
+
+        st.subheader("Предсказанные теги: ")
+        for tag in tags:
+            st.markdown(f"- **{tag}**")
+        
+        st.subheader("Вероятность по тегам: ")
+        # st.table(df_scores)
+        st.dataframe(
+            df_scores.style.background_gradient(cmap='gist_grey', subset=['Вероятность'])
+        )
+    else:
+        st.warning("Сначала введите текст")
